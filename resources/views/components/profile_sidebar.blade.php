fa22a1d55a2b5fc810d96aee49503432bff74a47
wolfricoz <ricoisdenaam@gmail.com>
2024-01-16 04:33:08 +0100

    Added the profile sidebars to the profile, and fixed some errors in the SettingsController.php

--- resources/views/components/profile_sidebar.blade.php
+++ resources/views/components/profile_sidebar.blade.php
@@ -18,7 +18,7 @@
         :show="$user->profile->show_location"
         :label="('Country')"
     >
-        {{ $user->profile->location }}
+        {{ $user->profile->location ?? 'No information provided.' }}
     </x-public_profile_item>
 
     <div class="my-2 rounded-full border-b border-gray-300">
@@ -28,50 +28,50 @@
         :show="$user->profile->show_discord"
         :label="('Discord')"
     >
-        {{ $user->username }}
+        {{ $user->username ?? 'No information provided.' }}
     </x-public_profile_item>
     <x-public_profile_item
         :show="$user->profile->show_email"
         :label="('Email')"
     >
-        {{ $user->email }}
+        {{ $user->email ?? 'No information provided.' }}
     </x-public_profile_item>
 
     <x-public_profile_item
         :show="$user->profile->show_reddit"
         :label="('Twitter')"
     >
-        {{ $user->profile->twitter }}
+        {{ $user->profile->twitter ?? 'No information provided.' }}
     </x-public_profile_item>
     <x-public_profile_item
         :show="$user->profile->show_reddit"
         :label="('Reddit')"
     >
-        {{ $user->profile->reddit }}
+        {{ $user->profile->reddit ?? 'No information provided.' }}
     </x-public_profile_item>
     <x-public_profile_item
         :show="$user->profile->show_reddit"
         :label="('Reddit')"
     >
-        {{ $user->profile->reddit }}
+        {{ $user->profile->reddit ?? 'No information provided.' }}
     </x-public_profile_item>
     <x-public_profile_item
         :show="$user->profile->show_telegram"
         :label="('Telegram')"
     >
-        {{ $user->profile->telegram }}
+        {{ $user->profile->telegram ?? 'No information provided.' }}
     </x-public_profile_item>
     <x-public_profile_item
         :show="$user->profile->show_other"
         :label="('Other')"
     >
-        {{ $user->profile->other }}
+        {{ $user->profile->other ?? 'No information provided.' }}
     </x-public_profile_item>
     <x-public_profile_item
         :show="$user->profile->show_website"
         :label="('Website')"
     >
-        {{ $user->profile->website }}
+        {{ $user->profile->website ?? 'No information provided.' }}
     </x-public_profile_item>
     @if ($user->id === auth()->user()->id)
         <p class="pt-3 text-xs">
