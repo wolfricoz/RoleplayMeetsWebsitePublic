c914c72d4d1390e2c6dc8caee354ec712df5c6c5
wolfricoz <ricoisdenaam@gmail.com>
2024-03-12 20:41:16 +0100

    Changed genre location, hid the approved status

--- resources/views/components/layout/SVG/check-icon.blade.php
+++ resources/views/components/layout/SVG/check-icon.blade.php
@@ -14,19 +14,21 @@
       SFW
     </span>
   @endif
-  @if ($post->approved)
-    <span
-      class="h6 h-6 cursor-default rounded-full bg-green-500 px-1.5 text-sm"
-      title="Approved"
-    >
-      Approved
-    </span>
-  @else
-    <span
-      class="h-6 cursor-default rounded-full bg-red-400 px-1.5 text-sm"
-      title="Not approved"
-    >
-      Awaiting approval
-    </span>
+  @if(Route::is(["admin.*", 'dashboard', 'posts.show']))
+    @if ($post->approved)
+      <span
+        class="h6 h-6 cursor-default rounded-full bg-green-500 px-1.5 text-sm"
+        title="Approved"
+      >
+    Approved
+  </span>
+    @else
+      <span
+        class="h-6 cursor-default rounded-full bg-red-400 px-1.5 text-sm"
+        title="Not approved"
+      >
+    Awaiting approval
+  </span>
+    @endif
   @endif
 </div>
